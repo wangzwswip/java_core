4a7b044ccc982851d625e91a4fedaf3ad214d029
wangzw <wang410@gmail.com>
2019-12-02 22:35:07 +0800

    modify chapter11 first ActionTest class PerformTest commit action chapter12 calculater

---
+++ chapter12/src/CalculatorTest.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(()-> {
+            JFrame frame = new CalculateFrame();
+            frame.setTitle("Calculate-test");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
+class CalculateFrame extends JFrame {
+    public CalculateFrame() {
+        add(new CalculatorPanel());
+        pack();
+    }
+}
