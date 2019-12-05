6cabb6b1321c4309a15b5e546c33e77eb3e2b05c
wangzw <wang410@gmail.com>
2019-12-05 22:54:05 +0800

    modify chapter11 first ActionTest class PerformTest commit action chapter12 calculater

--- chapter12/src/CalculatorTest.java
+++ chapter12/src/CalculatorTest.java
@@ -4,16 +4,18 @@ import java.awt.*;
 public class CalculatorTest {
     public static void main(String[] args) {
         EventQueue.invokeLater(()-> {
-            JFrame frame = new CalculateFrame();
+            JFrame frame = new SliderFrame();
             frame.setTitle("Calculate-test");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setVisible(true);
         });
     }
 }
+/*
 class CalculateFrame extends JFrame {
     public CalculateFrame() {
         add(new CalculatorPanel());
         pack();
     }
 }
+ */
---
+++ chapter12/src/ComboBoxFrame.java
@@ -0,0 +1,41 @@
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.*;
+
+/**
+ * A frame with a sample text label and a combo box for selecting font faces.
+ */
+
+public class ComboBoxFrame extends JFrame {
+    private JComboBox<String> faceCombo;
+    private JLabel label;
+    private static final int DEFAULT_SIZE = 24;
+
+    public ComboBoxFrame() {
+        //ADD the sample text label
+        label = new JLabel("The quick brown fox jumps over the lazy dog.");
+        label.setFont(new Font("Serif",Font.PLAIN,DEFAULT_SIZE));
+        add(label,BorderLayout.CENTER);
+
+        //Make a combo box and add face names
+        faceCombo = new JComboBox<>();
+        faceCombo.addItem("Serif");
+        faceCombo.addItem("SansSerif");
+        faceCombo.addItem("Monospaced");
+        faceCombo.addItem("Dialog");
+        faceCombo.addItem("DialogInput");
+
+        //the combo box listener changes the label font to the selected face name
+
+        faceCombo.addActionListener(event ->
+                label.setFont(
+                        new Font(faceCombo.getItemAt(faceCombo.getSelectedIndex()),
+                                Font.PLAIN,DEFAULT_SIZE)));
+        //add combo box to a panel at the frame's southern border
+        JPanel comboPanel = new JPanel();
+        comboPanel.add(faceCombo);
+        add(comboPanel,BorderLayout.SOUTH);
+        pack();
+    }
+}
